d011d50902ec5c9b416e56264fea31df1d7b564c
Dennis <dennismusembi344@gmail.com>
2024-02-19 23:32:06 +0300

    done2

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ const tutorials = [
 ];
 
 
-function titleCased(tutorial){
-  return tutorials.map(tutorial=>{
-  return tutorial.split(" ").map((word)=> {
-    return word[0].toUpperCase() + word.slice(1);
-  }).join(" ");
-  });}
+// function titleCased(tutorial){
+//   return tutorials.map(tutorial=>{
+//   return tutorial.split(" ").map((word)=> {
+//     return word[0].toUpperCase() + word.slice(1);
+//   }).join(" ");
+//   });}
+const titleCased = () => {
+  return tutorials.map(tutorial => {
+    return tutorial.replace(/\b\w/g, match => match.toUpperCase());
+  });
+};
